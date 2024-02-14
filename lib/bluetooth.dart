747be8ca0ec76c8fbd969446a041bfde72068bc3
vikas4003 <210070093@iitb.ac.in>
2024-02-14 16:26:11 +0530

    bluetooth scanning

---
+++ lib/bluetooth.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+class Bluetooth extends StatefulWidget {
+  const Bluetooth({super.key});
+
+  @override
+  State<Bluetooth> createState() => _BluetoothState();
+}
+
+class _BluetoothState extends State<Bluetooth> {
+  // FlutterBluePlus flutterBlue = FlutterBluePlus.instance;
+  List<BluetoothDevice> devices = [];
+
+  @override
+  void initState() {
+    super.initState();
+    print("init state");
+    startScanning();
+  }
+
+  void startScanning() async {
+    print("waiting for scan");
+    await FlutterBluePlus.startScan();
+    print("done with scan");
+    FlutterBluePlus.scanResults.listen((results) {
+      for (ScanResult result in results) {
+        if (!devices.contains(result.device)) {
+          setState(() {
+            devices.add(result.device);
+            print(result.device);
+          });
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BLE Scanner'),
+      ),
+      body: ListView.builder(
+        itemCount: devices.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            // ignore: deprecated_member_use
+            title: Text(devices[index].platformName),
+            subtitle: Text(devices[index].remoteId.toString()),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 // import 'package:edl_app/home.dart';
 // import 'package:edl_app/loading.dart';
 import 'package:edl_app/login.dart';
+import 'package:edl_app/bluetooth.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: '/login',
+    initialRoute: '/bluetooth',
     routes: {
       // '/': (context) => Loading(),
       // '/home': (context) => Home(),
       '/login': (context) => Login(),
+      '/bluetooth': (context) => Bluetooth(),
     },
   ));
   // minor change
